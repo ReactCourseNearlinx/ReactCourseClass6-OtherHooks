1f499b4f7b27189cca68f4d0d3d271ea28fafdd2
SamSerra7 <samsserranoguerra7@gmail.com>
2022-08-28 01:24:11 -0600

    Add memo, useMemo, useCallback, useRef logic

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,5 +1,7 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, memo, useMemo } from 'react';
 import data from "./api/index";
+
+const BIG_NUMBER = 5_000_000_000;
 
 const styles = {
   textAlign:'center', 
@@ -9,17 +11,33 @@ const styles = {
   padding: '1em',
   width: '50%',
   height: '50%',
-}
+};
 
-const Child = () => {
+const inputStyles = {
+  width:'80%', 
+  margin:'0.5rem auto', 
+  height:'2rem', 
+  borderRadius:'5px',
+  cursor: 'pointer',
+};
+
+const Child = (props) => {
 
   const [currentCar, setCurrentCar] = useState('');
   const totalRenders = useRef(0);
 
+  const expensiveCalculation = useMemo(()=> {
+    let totalInfo = 0;
+    for (let i = 0; i < BIG_NUMBER; i++) {
+      totalInfo++;
+    }
+    return totalInfo;
+  },[])
+
   return (
     <div style={styles}>
       
-      <select value={currentCar} onChange={(e)=>setCurrentCar(e.target.value)}>
+      <select value={currentCar} onChange={(e)=>setCurrentCar(e.target.value)} style={inputStyles}>
         {[...data].map((car)=>
           <option key={car[0]+car[1]}  value={car[0]}>
             {car[1]}
@@ -27,10 +45,12 @@ const Child = () => {
         )}
       </select>
       
+      <button style={inputStyles} onClick={props.updatePage}>Return Page: {props.page}</button>
+      
       <h2>Total Child Renders: {totalRenders.current++}</h2>
-
+      <h3>Expensive calculation result: {expensiveCalculation}</h3>
     </div>
   )
-}
+};
 
-export default Child;
+export default memo(Child);
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,5 +1,5 @@
-import { useState, useRef } from 'react'
-import Child from './Child'
+import { useState, useRef, useCallback } from 'react';
+import Child from './Child';
 
 const styles = {
   textAlign:'center', 
@@ -9,20 +9,40 @@ const styles = {
   padding: '1em',
   width: '50%',
   height: '400px',
-}
+};
+
+const inputTextStyle = {
+  width:'90%', 
+  margin:'auto', 
+  height:'2rem', 
+  borderRadius:'5px'
+};
+
+const buttonStyle = {
+  width:'20%', 
+  margin:'0.5rem auto', 
+  height:'2rem', 
+  borderRadius:'5px',
+  cursor: 'pointer',
+};
+
 
 const Parent = () => {
 
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  const [page, setPage] = useState(0);
   const totalRenders = useRef(0);
+
+  const returnPage = useCallback(() => setPage(page-1), [page]);
 
   return (
     <div style={styles}>
         <h2>Total Parent Renders: {totalRenders.current++}</h2>
-        <input value={text} onChange={e => setText(e.target.value)} style={{width:'90%', margin:'auto', height:'2rem', borderRadius:'5px'}}/>
-        <Child />
+        <input value={text} onChange={e => setText(e.target.value)} style={inputTextStyle}/>
+        <button onClick={() => setPage(page+1)} style={buttonStyle}>Next Page: {page}</button>
+        <Child page={page} updatePage={returnPage} />
     </div>
   )
-}
+};
 
 export default Parent;
